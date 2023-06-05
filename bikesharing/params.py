aa371a3d5a85d425d637ddc41b9809dcfafe3758
Sandra Hofer <sandra_hoefer@gmx.de>
2023-06-05 21:58:17 +0200

    updated params with google cloud params

--- bikesharing/params.py
+++ bikesharing/params.py
@@ -1,6 +1,14 @@
 import os
 import numpy as np
 
-### preprocessing params ####
+#### preprocessing params ####
 START_YEAR = int(os.environ.get("START_YEAR"))
 END_YEAR = int(os.environ.get("END_YEAR"))
+
+########### GCP ##############
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+BQ_DATASET = os.environ.get("BQ_DATASET")
+BQ_REGION = os.environ.get("BQ_REGION")
+
+########### CONSTANTS ###########
+LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), ".lewagon", "bikesharing", "data")

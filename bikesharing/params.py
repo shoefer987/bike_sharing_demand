3d6de381370964c777617141240d419b13230e31
Sandra Hofer <sandra_hoefer@gmx.de>
2023-06-05 17:23:19 +0200

    implemented get_raw_data function

--- bikesharing/params.py
+++ bikesharing/params.py
@@ -2,5 +2,5 @@ import os
 import numpy as np
 
 ### preprocessing params ####
-START_YEAR = os.environ.get("START_YEAR")
-END_YEAR = os.environ.get("END_YEAR")
+START_YEAR = int(os.environ.get("START_YEAR"))
+END_YEAR = int(os.environ.get("END_YEAR"))

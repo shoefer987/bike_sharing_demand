323aa73c1c53825fe088af9ad77b27b48b0d9c68
Sandra Hofer <sandra_hoefer@gmx.de>
2023-06-06 18:01:17 +0200

    added outline for preprocessing

--- bikesharing/ml_logic/preprocessor.py
+++ bikesharing/ml_logic/preprocessor.py
@@ -1,3 +1,21 @@
 import numpy as np
 import pandas as pd
 from bikesharing.params import *
+
+# get data from BQ -> data.get_raw_data
+
+# get y from geocoordinates & label encode it
+
+# get weather data -> data.get_weather_data
+
+# aggregate BQ data by hour
+
+# join BQ-data & weather data
+
+# flag weekends & holidays
+
+# encode day/month/hour (-> sin/cos)
+
+# drop everything BUT weather data and engineered features
+
+# scale numeric features

101821f39e1ec1b15e90b2c8854d8d1f1852c493
Jui Kate <juikate91@gmail.com>
2023-06-07 16:32:35 +0200

    Holiday and Weekend Flag Implemented

---
+++ bikesharing/ml_logic/feature_engineering.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import holidays
+from datetime import date
+
+# Function for Holiday Flag
+def is_holiday(data: pd.DataFrame):
+    """
+    Performs feature engineering on the input data.
+
+    Args:
+        data (pd.DataFrame): The input DataFrame.
+
+    Returns:
+        DataFrame: A DataFrame containing the Holiday Flags.
+    """
+    data['rent_date_hour'] = pd.to_datetime(data['rent_date_hour'])
+
+    # Extract date from rent_date_hour
+    X = data['rent_date_hour'].dt.date
+
+    # Checking for the Bayern Holidays
+    bay_holidays = holidays.CountryHoliday('DE', prov='BY')
+
+    data['is_holiday'] = X.apply(lambda x: 1 if x in bay_holidays else 0)
+
+    return data[['rent_date_hour', 'is_holiday']]
+
+
+# Function for Weekend Flag
+def is_weekend(data: pd.DataFrame):
+    """
+    Performs feature engineering on the input data.
+
+    Args:
+        data (pd.DataFrame): The input DataFrame.
+
+    Returns:
+        DataFrame: A DataFrame containing the Weekend Flags.
+    """
+    data['rent_date_hour'] = pd.to_datetime(data['rent_date_hour'])
+
+    # Extract date from rent_date_hour
+    X = data['rent_date_hour'].dt.date
+
+    # Checking if Day is a Weekend or not
+    data["is_weekend"] = X.apply(lambda x: 1 if x.weekday() >= 5 else 0)
+
+    return data[['rent_date_hour', 'is_weekend']]
